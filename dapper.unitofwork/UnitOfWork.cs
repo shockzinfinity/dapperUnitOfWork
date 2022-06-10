83818c814b735b6963177b74c2eddaf92b996886
Jun Yu <maddreamer@nate.com>
2022-06-10 17:54:25 +0900

    nullable fix

--- dapper.unitofwork/UnitOfWork.cs
+++ dapper.unitofwork/UnitOfWork.cs
@@ -5,12 +5,12 @@ namespace dapper.unitofwork;
 public class UnitOfWork : IUnitOfWork
 {
   private bool _disposed;
-  private IDbConnection _connection;
-  private readonly RetryOptions _retryOptions;
+  private IDbConnection? _connection;
+  private readonly RetryOptions? _retryOptions;
   private readonly CancellationToken _cancellationToken;
-  private IDbTransaction _transaction;
+  private IDbTransaction? _transaction = null;
 
-  public UnitOfWork(IDbConnection connection, bool transactional = false, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted, RetryOptions retryOptions = null, CancellationToken cancellationToken = default)
+  public UnitOfWork(IDbConnection connection, bool transactional = false, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted, RetryOptions? retryOptions = null, CancellationToken cancellationToken = default)
   {
     _connection = connection;
     _retryOptions = retryOptions;
@@ -20,20 +20,20 @@ public class UnitOfWork : IUnitOfWork
       _transaction = connection.BeginTransaction(isolationLevel);
   }
 
-  public T Query<T>(IQuery<T> query) => Retry.Do(() => query.Execute(_connection, _transaction), _retryOptions);
+  public T Query<T>(IQuery<T> query) => Retry.Do(() => query.Execute(_connection!, _transaction!), _retryOptions!);
 
   public Task<T> QueryAsync<T>(IAsyncQuery<T> query)
-    => Retry.DoAsync(() => query.ExecuteAsync(_connection, _transaction, _cancellationToken), _retryOptions);
+    => Retry.DoAsync(() => query.ExecuteAsync(_connection!, _transaction!, _cancellationToken), _retryOptions!);
 
   public Task<T> QueryAsync<T>(IAsyncQuery<T> query, CancellationToken cancellationToken)
-    => Retry.DoAsync(() => query.ExecuteAsync(_connection, _transaction, cancellationToken), _retryOptions);
+    => Retry.DoAsync(() => query.ExecuteAsync(_connection!, _transaction!, cancellationToken), _retryOptions!);
 
   public void Execute(ICommand command)
   {
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    Retry.Do(() => command.Execute(_connection, _transaction), _retryOptions);
+    Retry.Do(() => command.Execute(_connection!, _transaction!), _retryOptions!);
   }
 
   public T Execute<T>(ICommand<T> command)
@@ -41,7 +41,7 @@ public class UnitOfWork : IUnitOfWork
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    return Retry.Do(() => command.Execute(_connection, _transaction), _retryOptions);
+    return Retry.Do(() => command.Execute(_connection!, _transaction!), _retryOptions!);
   }
 
   public Task ExecuteAsync(IAsyncCommand command)
@@ -49,7 +49,7 @@ public class UnitOfWork : IUnitOfWork
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    return Retry.DoAsync(() => command.ExecuteAsync(_connection, _transaction, _cancellationToken), _retryOptions);
+    return Retry.DoAsync(() => command.ExecuteAsync(_connection!, _transaction!, _cancellationToken), _retryOptions!);
   }
 
   public Task ExecuteAsync(IAsyncCommand command, CancellationToken cancellationToken)
@@ -57,7 +57,7 @@ public class UnitOfWork : IUnitOfWork
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    return Retry.DoAsync(() => command.ExecuteAsync(_connection, _transaction, cancellationToken), _retryOptions);
+    return Retry.DoAsync(() => command.ExecuteAsync(_connection!, _transaction!, cancellationToken), _retryOptions!);
   }
 
   public Task<T> ExecuteAsync<T>(IAsyncCommand<T> command)
@@ -65,7 +65,7 @@ public class UnitOfWork : IUnitOfWork
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    return Retry.DoAsync(() => command.ExecuteAsync(_connection, _transaction, _cancellationToken), _retryOptions);
+    return Retry.DoAsync(() => command.ExecuteAsync(_connection!, _transaction!, _cancellationToken), _retryOptions!);
   }
 
   public Task<T> ExecuteAsync<T>(IAsyncCommand<T> command, CancellationToken cancellationToken)
@@ -73,7 +73,7 @@ public class UnitOfWork : IUnitOfWork
     if (command.RequiresTransaction && _transaction == null)
       throw new Exception($"This command {command.GetType()} requires a transaction");
 
-    return Retry.DoAsync(() => command.ExecuteAsync(_connection, _transaction, cancellationToken), _retryOptions);
+    return Retry.DoAsync(() => command.ExecuteAsync(_connection!, _transaction!, cancellationToken), _retryOptions!);
   }
 
   public void Commit() => _transaction?.Commit();
